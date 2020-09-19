e55f687542c8566295b49255c9a681f1adeb9338
Lactore <andre.schnelzer@yahoo.de>
2020-09-19 09:31:20 +0000

    Add multiple kitties

--- pallets/kitties/src/lib.rs
+++ pallets/kitties/src/lib.rs
@@ -45,7 +45,15 @@ decl_storage! {
 	trait Store for Module<T: Trait> as Kitties {
 		Kitties get(fn kitty): map hasher(blake2_128_concat) T::Hash => Kitty<T::Hash, T::Balance>;
 		KittyOwner get(fn owner_of): map hasher(blake2_128_concat) T::Hash => Option<T::AccountId>;
-		OwnedKitty get(fn kitty_of_owner): map hasher(blake2_128_concat) T::AccountId => T::Hash;
+
+		AllKittiesArray get(fn kitty_by_index): map hasher(blake2_128_concat) u64 => T::Hash;
+		AllKittiesCount get(fn all_kitties_count): u64;
+		AllKittiesIndex: map hasher(blake2_128_concat) T::Hash => u64;
+
+		OwnedKittiesArray get(fn kitty_of_owner_by_index): map hasher(blake2_128_concat) (T::AccountId, u64) => T::Hash;
+		OwnedKittiesCount get(fn owned_kitty_count): map hasher(blake2_128_concat) T::AccountId => u64;
+		OwnedKittiesIndex: map hasher(blake2_128_concat) T::Hash => u64;
+
 		Nonce: u64;
 	}
 }
@@ -62,6 +70,18 @@ decl_module! {
 		#[weight = 10_000]
 		fn create_kitty(origin) -> DispatchResult {
 			let sender = ensure_signed(origin)?;
+
+			let owned_kitty_count = Self::owned_kitty_count(&sender);
+			let new_owned_kitty_count = owned_kitty_count.checked_add(1)
+			.ok_or("Overflow adding a new kitty to account balance")?;
+
+			// Get Kitties count
+			let all_kitties_count = Self::all_kitties_count();
+
+			let new_all_kitties_count = all_kitties_count.checked_add(1)
+			.ok_or("Overflow adding a new kitty to total supply")?;
+
+
 			let random_hash = <pallet_randomness_collective_flip::Module<T>>::random(&b"my context"[..]).using_encoded(<T as frame_system::Trait>::Hashing::hash);
 
 			ensure!(!KittyOwner::<T>::contains_key(random_hash), Error::<T>::KittyAlreadyExists);
@@ -75,7 +95,14 @@ decl_module! {
 
 			Kitties::<T>::insert(random_hash, new_kitty);
 			KittyOwner::<T>::insert(random_hash, &sender);
-			OwnedKitty::<T>::insert(&sender, random_hash);
+
+			AllKittiesArray::<T>::insert(all_kitties_count,random_hash);
+			AllKittiesCount::put(new_all_kitties_count);
+			AllKittiesIndex::<T>::insert(random_hash, all_kitties_count);
+
+			OwnedKittiesArray::<T>::insert((sender.clone(), owned_kitty_count),random_hash);
+			OwnedKittiesCount::<T>::insert(&sender, new_owned_kitty_count);
+			OwnedKittiesIndex::<T>::insert(random_hash, owned_kitty_count);
 
 			Nonce::mutate(|x| *x += 1);
 
